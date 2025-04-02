ecdc34c0500c6ca2888400c4927a8e5267b0abe9
rivo pelu <rivopelu12@gmail.com>
2025-04-02 23:03:40 +0700

    FEAT:add migration

--- src/db/base-entity.ts
+++ src/db/base-entity.ts
@@ -3,9 +3,9 @@ import * as t from 'drizzle-orm/mysql-core';
 export const baseEntity = {
   active: t.boolean().default(true).notNull(),
   createdDate: t.timestamp('created_date').notNull().defaultNow(),
-  createdBy: t.varchar({ length: 256 }).notNull(),
+  createdBy: t.varchar('created_by', { length: 256 }).notNull(),
   updatedDate: t.timestamp('updated_date'),
-  updatedBy: t.varchar({ length: 256 }),
-  deletedBy: t.varchar({ length: 256 }),
+  updatedBy: t.varchar('updated_by', { length: 256 }),
+  deletedBy: t.varchar('deleted_by', { length: 256 }),
   deletedDate: t.timestamp('deleted_date'),
 };
---
+++ src/db/migrations/0002_lazy_stark_industries.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `account` RENAME COLUMN `createdBy` TO `created_by`;--> statement-breakpoint
+ALTER TABLE `account` RENAME COLUMN `updatedBy` TO `updated_by`;--> statement-breakpoint
+ALTER TABLE `account` RENAME COLUMN `deletedBy` TO `deleted_by`;
