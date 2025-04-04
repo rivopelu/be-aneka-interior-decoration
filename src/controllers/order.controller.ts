34971114bf8daf58eaac6c82aa8459d470eb94e4
rivo pelu <rivopelu12@gmail.com>
2025-04-04 20:19:30 +0700

    FEAT:add add api for check shipping fee

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -3,6 +3,7 @@ import { HttpService } from '../services/http.service';
 import { ENV } from '../constants/env';
 import { EXTERNAL_ENDPOINT } from '../constants/endpoint';
 import { BadRequestError } from '../utils/error';
+import { IResCheckDeliveryFee } from '../types/response/IResCheckDeliveryFee';
 
 export class OrderController {
   async getShippingDestination(
@@ -22,6 +23,40 @@ export class OrderController {
         EXTERNAL_ENDPOINT.GET_DESTINATION(q),
       );
       res.data(response.data?.data?.sicepat?.results);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  async cekDeliveryFee(req: Request, res: Response, next: NextFunction) {
+    const httpService = new HttpService(ENV.SHIPPING_SERVICE_ENDPOINT);
+    const destinationCode = req.query.code;
+    if (!destinationCode) {
+      throw new BadRequestError('Destination Code is required');
+    }
+    const data = {
+      origin: 'JOG',
+      destination: 'MDC20400',
+      weight: '1',
+      p: '12',
+      l: '12',
+      t: '12',
+    };
+    const resCheck = await httpService.POST(
+      EXTERNAL_ENDPOINT.CHECK_DELIVERY_FEE(),
+      data,
+    );
+    const parseData: any[] = resCheck.data?.data?.sicepat?.results || [];
+    const resData: IResCheckDeliveryFee[] = parseData.map((e) => {
+      return {
+        cost: e?.tariff,
+        service_name: e?.service,
+        description: e?.description,
+        estimated: e?.etd,
+      };
+    });
+    try {
+      res.data(resData);
     } catch (e) {
       next(e);
     }
--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -5,5 +5,6 @@ import { OrderController } from '../controllers/order.controller';
 const router = Router();
 const controller = new OrderController();
 router.get('/v1/destination', verifyToken, controller.getShippingDestination);
+router.get('/v1/check-delivery-fee', verifyToken, controller.cekDeliveryFee);
 
 export const orderRoutes = router;
